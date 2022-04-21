9740ace524c1cdbc5d3c01f34a9a6d2f8682ff05
Koray Kutanoğlu <59109688+koraykutanoglu@users.noreply.github.com>
2022-04-21 16:18:38 +0300

    Delete instance-oslogin-member-iam.tf

--- create-vm-instances/instance-oslogin-member-iam.tf
+++
@@ -1,7 +0,0 @@
-resource "google_compute_instance_iam_member" "member" {
-  project = google_compute_instance.default.project
-  zone = google_compute_instance.default.zone
-  instance_name = google_compute_instance.default.name
-  role = "roles/compute.osLogin"
-  member = "user:koraykutan@gmail.com"
-}
